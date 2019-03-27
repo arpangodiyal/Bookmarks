c0a312df884ae68d01f236678ab5754432a539a2
arpangodiyal24@gmail.com <arpangodiyal24@gmail.com>
2019-03-27 11:38:12 +0530

    Messages

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Profile
 
 def register(request):
@@ -17,7 +18,9 @@ def register(request):
             # Save the User object
             new_user.save()
             profile = Profile.objects.create(user = new_user)
+            messages.success(request, 'User registered successfully')
             return render(request,'registerDone.html',{'new_user': new_user})
+        messages.error(request, 'Something went wrong')
     else:
         user_form = UserRegistrationForm()
     return render(request,'register.html',{'user_form': user_form})
@@ -34,7 +37,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
             return redirect('dashboard')
+        messages.error(request, 'Something went wrong')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
